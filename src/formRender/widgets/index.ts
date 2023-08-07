e3c259bef2af9748c00cbfecf30ea660cc4288e3
yangyanbo <2064825358@qq.com>
2023-08-07 16:46:29 +0800

    style: 移动field文件夹

--- src/formRender/widgets/index.ts
+++ src/formRender/widgets/index.ts
@@ -1,5 +1,5 @@
-export { default as Input } from './input/input.tsx';
-export type { InputProps } from './input/input.tsx';
-export { default as InputNumber } from './inputNumber/inputNumber';
-export type { InputNumberProps } from './inputNumber/inputNumber';
-export { default as Select } from './select/select.vue';
+export { default as Input } from './fields/input/input.tsx';
+export type { InputProps } from './fields/input/input.tsx';
+export { default as InputNumber } from './fields/inputNumber/inputNumber';
+export type { InputNumberProps } from './fields/inputNumber/inputNumber';
+export { default as Select } from './fields/select/select.vue';
